2661754a9444849ee4e89aa7545266f9c3bf9b48
Nelson <tomneo2004@gmail.com>
2019-09-12 21:25:40 +0800

    add comment to passport and passportAuth

--- passport/passport.js
+++ passport/passport.js
@@ -10,15 +10,17 @@ passport.use('login', new localStrategy({
 
   (email, password, done)=>{
 
+    //check if user exist
     let userExist = userData.find(user=>(user.email===email && user.password===password));
 
+    //user exist
     if(userExist){
         return done(null, userExist, {
             message:'login succssful',
             error:null
         });
     }
-    else{
+    else{//user does not exist
         return done(null, false,{
             message:'user not found',
             error:null
--- passport/passportAuth.js
+++ passport/passportAuth.js
@@ -2,16 +2,21 @@ const passport = require('passport');
 
 const userAuth = (req, res, next)=>{
 
-    passport.authenticate('login', (err, user, info)=>{
+    //call passport authenticate. passport will use your strategy
+    //we don't want passport to use session
+    passport.authenticate('login', {session:false}, (err, user, info)=>{
 
+        //if any error occured
         if(err){
             return res.status(400).json({...{status:'Fail'}, info});
         }
 
+        //if user dose not exist
         if(!user){
             return res.status(400).json({...{status:'Fail'}, info});
         }
 
+        //user exist and no error then log user in
         return res.status(200).json({...{status:'OK'}, info});
 
 
