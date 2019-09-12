339dcf8db0bad2e4c656e9e652cccf0da39a2c1d
Nelson <tomneo2004@gmail.com>
2019-09-12 15:09:50 +0800

    Add express-session and add redis, use redis as storage for express-session

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express  = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const redis = require('redis');
+
 
 const app = express();
+
+let RedisStore = require('connect-redis')(session);
+let redisClient = redis.createClient();
+
+redisClient.on('error', console.error);
+
+const sessConfig = {
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        httpOnly: true, 
+        secure: false,
+    },
+    store:new RedisStore({client:redisClient, ttl:3 })
+}
+
+app.use(session(sessConfig));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,8 +31,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{
-
-    res.send('Server is working');
+    res.send('Server is working and your session id is '+req.sessionID);
 })
 
 app.listen(3000, ()=>{
